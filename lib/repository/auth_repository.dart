0599d7ea34d41e10e4468bacb355bd470a5f8029
Muhammad Hamza <hamzamee2020@gmail.com>
2023-11-16 10:07:57 +0500

    first commit

---
+++ lib/repository/auth_repository.dart
@@ -0,0 +1,42 @@
+
+
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:mvvm/data/network/BaseApiServices.dart';
+import 'package:mvvm/data/network/NetworkApiService.dart';
+import 'package:mvvm/data/network/base_api_services.dart';
+import 'package:mvvm/data/network/network_api_services.dart';
+import 'package:mvvm/res/app_url.dart';
+
+class AuthRepository  {
+
+  BaseApiServices _apiServices = NetworkApiService() ;
+
+
+  Future<dynamic> loginApi(dynamic data )async{
+
+    try{
+
+      dynamic response = await _apiServices.getPostApiResponse(AppUrl.loginEndPint, data);
+      return response ;
+
+    }catch(e){
+      throw e ;
+    }
+  }
+
+  Future<dynamic> signUpApi(dynamic data )async{
+
+    try{
+
+      dynamic response = await _apiServices.getPostApiResponse(AppUrl.registerApiEndPoint, data);
+      return response ;
+
+
+    }catch(e){
+      throw e ;
+    }
+  }
+
+}
